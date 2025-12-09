0752ee6004c06bc8c7c68bd292fed6f05412407f
VesMaybeVesper <vesmaybevesper@gmail.com>
2025-12-09 18:16:45 -0500

    Create clientlevel mixin using old dist

---
+++ src/main/java/dev/vesper/substrate/mixin/ClientLevelMixin.java
@@ -0,0 +1,41 @@
+package dev.vesper.substrate.mixin;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.core.SectionPos;
+import net.minecraft.world.level.ChunkPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.util.perf.Profiler;
+
+import static dev.vesper.substrate.Substrate.ceilingY;
+import static dev.vesper.substrate.Substrate.enabled;
+import static dev.vesper.substrate.Substrate.floorY;
+import static dev.vesper.substrate.Substrate.serverDisabled;
+
+@Mixin(ClientLevel.class)
+public class ClientLevelMixin {
+	@Inject(method = "onChunkLoaded", at = @At("RETURN"))
+	private void onChunkLoad(ChunkPos chunkPos, CallbackInfo ci){
+		if (!enabled.get() || serverDisabled.get()) return;
+
+		if (Minecraft.getInstance().levelRenderer != null){
+			int dist = 2; //(int) Minecraft.getInstance().levelRenderer.getLastViewDistance() + 1
+
+			for (int x = chunkPos.x - dist; x <= chunkPos.x + dist; x++){
+				for (int z = chunkPos.z - dist; z <= chunkPos.z + dist; z++){
+					if (floorY.get() != Integer.MIN_VALUE){
+						int sy = SectionPos.blockToSectionCoord(floorY.get());
+						Minecraft.getInstance().levelRenderer.setSectionDirtyWithNeighbors(x, sy, z);
+					}
+					if (ceilingY.get() != Integer.MAX_VALUE){
+						int sy = SectionPos.blockToSectionCoord(ceilingY.get());
+						Minecraft.getInstance().levelRenderer.setSectionDirtyWithNeighbors(x, sy, z);
+					}
+				}
+			}
+		}
+	}
+}
